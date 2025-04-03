4f9608ce11fd949936d2db7ba69f1e4fe146736b
Willy <wacb14@gmail.com>
2025-04-02 19:41:54 -0500

    Add recent entries section

--- src/app/main-dashboard/main-dashboard.component.ts
+++ src/app/main-dashboard/main-dashboard.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
-import { RadialProgressBarComponent } from "../components/radial-progress-bar/radial-progress-bar.component";
+import { RadialProgressBarComponent } from '../components/radial-progress-bar/radial-progress-bar.component';
+
+interface Entry {
+  id: number;
+  food: string;
+  protein: number;
+  carbs: number;
+  fats: number;
+  dateTime: Date;
+  icon: string;
+}
 
 @Component({
   selector: 'app-main-dashboard',
@@ -8,4 +18,69 @@ import { RadialProgressBarComponent } from "../components/radial-progress-bar/ra
   styleUrl: './main-dashboard.component.css',
 })
 export class MainDashboardComponent {
+  recentEntries: Entry[] = [
+    {
+      id: 1,
+      food: 'Pechuga de pollo',
+      protein: 31,
+      carbs: 0,
+      fats: 3.6,
+      dateTime: new Date('2023-05-15T12:30:00'),
+      icon: 'ep:food',
+    },
+    {
+      id: 2,
+      food: 'Arroz blanco',
+      protein: 2.7,
+      carbs: 28,
+      fats: 0.3,
+      dateTime: new Date('2023-05-15T13:15:00'),
+      icon: 'ep:food',
+    },
+    {
+      id: 3,
+      food: 'Aguacate',
+      protein: 2,
+      carbs: 9,
+      fats: 15,
+      dateTime: new Date('2023-05-16T08:45:00'),
+      icon: 'ep:food',
+    },
+    {
+      id: 4,
+      food: 'Huevos revueltos',
+      protein: 12,
+      carbs: 1,
+      fats: 10,
+      dateTime: new Date('2023-05-16T09:00:00'),
+      icon: 'ep:food',
+    },
+    {
+      id: 5,
+      food: 'Batido de proteína',
+      protein: 24,
+      carbs: 8,
+      fats: 2,
+      dateTime: new Date('2023-05-16T16:30:00'),
+      icon: 'ep:food',
+    },
+    {
+      id: 6,
+      food: 'Huevos revueltos',
+      protein: 12,
+      carbs: 1,
+      fats: 10,
+      dateTime: new Date('2023-05-16T09:00:00'),
+      icon: 'ep:food',
+    },
+    {
+      id: 7,
+      food: 'Batido de proteína',
+      protein: 24,
+      carbs: 8,
+      fats: 2,
+      dateTime: new Date('2023-05-16T16:30:00'),
+      icon: 'ep:food',
+    },
+  ];
 }
