9ab188e246d1735bfc907bfbcee94c0ba5142d6a
Willy <wacb14@gmail.com>
2025-04-06 22:52:33 -0500

    New component for search

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  imports: [],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.css',
+})
+export class SearchBarComponent {
+  @Input() placeholderMessage = 'Search...';
+}
