436e1c808ecfa0873c698baadefeb78899373aed
Willy <wacb14@gmail.com>
2025-04-06 22:55:52 -0500

    Design Search food component layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { SearchFoodComponent } from './search-food/search-food.component';
 
-export const routes: Routes = [{ path: '', component: MainDashboardComponent }];
+export const routes: Routes = [
+  { path: '', component: MainDashboardComponent },
+  { path: 'search-food', component: SearchFoodComponent },
+];
