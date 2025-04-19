c6a6b7f32dd021ea51e99ad7ab27a5ede1865fe8
Willy <wacb14@gmail.com>
2025-04-19 11:57:45 -0500

    Layout sign-up component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,11 @@ import { MyFoodsComponent } from './my-foods/my-foods.component';
 import { HealthReportComponent } from './health-report/health-report.component';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './layout/layout.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent },
   {
     path: '',
     component: LayoutComponent,
