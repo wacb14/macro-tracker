8fab2fe7b2b8ebb6877fc203dbf41709c8b69df5
Willy <wacb14@gmail.com>
2025-04-19 11:29:12 -0500

    Layout login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,20 @@ import { SearchFoodComponent } from './search-food/search-food.component';
 import { CustomFoodComponent } from './custom-food/custom-food.component';
 import { MyFoodsComponent } from './my-foods/my-foods.component';
 import { HealthReportComponent } from './health-report/health-report.component';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
-  { path: '', component: MainDashboardComponent },
-  { path: 'search-food', component: SearchFoodComponent },
-  { path: 'custom-food', component: CustomFoodComponent },
-  { path: 'my-foods', component:  MyFoodsComponent},
-  { path: 'health-report', component:  HealthReportComponent},
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: MainDashboardComponent },
+      { path: 'search-food', component: SearchFoodComponent },
+      { path: 'custom-food', component: CustomFoodComponent },
+      { path: 'my-foods', component: MyFoodsComponent },
+      { path: 'health-report', component: HealthReportComponent },
+    ],
+  },
 ];
